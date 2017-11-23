51c51691ea3bf29e13662b89beba41d2ab136f3e
WilliamTheBeginner <william78smart@hotmail.com>
2017-11-23 08:24:40 -0500

    People

--- star_wars_magic_methods/people.py
+++ star_wars_magic_methods/people.py
@@ -1,3 +1,21 @@
 class People(object):
     def __init__(self, name, dark_side=False):
-        pass
+        self.name = name
+        if self.name != type(str):
+        	raise TypeError()
+
+    def __str__(self):
+    	return self.name
+
+    def __call__(self):
+    	return 'Help me {}, you\'re my only hope.'.format(self.name)
+
+    def __div__(self, other):
+    	if self or other is type(object):
+
+    		return '{} throws a lightsaber at {}.'.format(self.name, other.name)
+    	else:
+    		raise TypeError()
+
+    def __mul__(self, other):
+    	pass
