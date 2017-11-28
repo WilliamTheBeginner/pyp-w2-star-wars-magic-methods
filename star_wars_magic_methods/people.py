2bbe4901f705e4b700b2692ac79caa07cf0b7193
WilliamTheBeginner <william78smart@hotmail.com>
2017-11-28 07:52:59 -0500

    New changes to people

--- star_wars_magic_methods/people.py
+++ star_wars_magic_methods/people.py
@@ -1,8 +1,6 @@
 class People(object):
-    def __init__(self, name, dark_side=False):
+    def __init__(self, name, dark_side=False, light_side=True):
         self.name = name
-        if self.name != type(str):
-        	raise TypeError()
 
     def __str__(self):
     	return self.name
@@ -18,4 +16,33 @@ class People(object):
     		raise TypeError()
 
     def __mul__(self, other):
-    	pass
+    	if self and other is type(object):
+
+    		return '{} throws a terminal detonator at {}!'.format(self.name, other.name)
+
+    def __rshift__(self, other):
+    	return '{} uses the force to push {} away form them'.format(self.name, other.name)
+
+    def __lshift__(self, other):
+    	return '{} uses the force to push {} towards them'.format(self.name, other.name)
+
+    def __neg__(self):
+    	return dark_side=True
+    	return light_side=False
+
+    def __pos__(self):
+    	return light_side=True
+    	return dark_side=False
+
+    def __invert__(self):
+    	if light_side = True and dark_side = False:
+    		return light_side = False and dark_side = True
+    	if light_side = False and dark_side = True:
+    		return light_side = True and dark_side = False
+
+    def __xor__(self, other):
+    	return '{} force chokes {}'.format(self.name, other.name)
+
+    def 
+
+    	
