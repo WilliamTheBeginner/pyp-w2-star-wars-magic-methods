51ee85275165b58997bab683d122824c3861c9da
William <william78smart@hotmail.com>
2017-11-29 14:14:58 +0000

    Finished people.py

--- star_wars_magic_methods/people.py
+++ star_wars_magic_methods/people.py
@@ -1,7 +1,12 @@
 class People(object):
     def __init__(self, name, dark_side=False, light_side=True):
         self.name = name
-
+        self.dark_side = dark_side
+        self.light_side = light_side
+        if self.dark_side == True:
+            self.light_side = False
+        if self.light_side == True:
+            self.dark_side = False
     def __str__(self):
     	return self.name
 
@@ -9,40 +14,60 @@ class People(object):
     	return 'Help me {}, you\'re my only hope.'.format(self.name)
 
     def __div__(self, other):
-    	if self or other is type(object):
+    	if isinstance(self, People) and isinstance(other, People):
 
-    		return '{} throws a lightsaber at {}.'.format(self.name, other.name)
+    		return '{} swings a lightsaber at {}.'.format(self.name, other.name)
     	else:
     		raise TypeError()
 
     def __mul__(self, other):
-    	if self and other is type(object):
+    	if isinstance(self, People) and isinstance(other, People):
 
-    		return '{} throws a terminal detonator at {}!'.format(self.name, other.name)
+    		return '{} throws a thermal detonator at {}!'.format(self.name, other.name)
+    		
+    	else:
+    	    raise TypeError()
 
     def __rshift__(self, other):
-    	return '{} uses the force to push {} away form them'.format(self.name, other.name)
+        if isinstance(self, People) and isinstance(other, People):
+            
+    	    return '{} uses the force to push {} away from them.'.format(self.name, other.name)
 
+        else:
+            raise TypeError()
+            
     def __lshift__(self, other):
-    	return '{} uses the force to push {} towards them'.format(self.name, other.name)
+    	if isinstance(self, People) and isinstance(other, People):
+    	
+    	    return '{} uses the force to pull {} towards them.'.format(self.name, other.name)
 
+        else: 
+            raise TypeError()
     def __neg__(self):
-    	return dark_side=True
-    	return light_side=False
+    	self.dark_side=True
+    	self.light_side=False
 
     def __pos__(self):
-    	return light_side=True
-    	return dark_side=False
+    	if self.dark_side == True:
+    	    self.light_side=True
+    	    self.dark_side=False
+    	
 
     def __invert__(self):
-    	if light_side = True and dark_side = False:
-    		return light_side = False and dark_side = True
-    	if light_side = False and dark_side = True:
-    		return light_side = True and dark_side = False
+    	self.light_side = not self.light_side
 
     def __xor__(self, other):
-    	return '{} force chokes {}'.format(self.name, other.name)
-
-    def 
-
+    	return '{} force chokes {}.'.format(self.name, other.name)
+    
+    def __eq__(self, other):
+        if other.name == 'Han Solo':
+            return 'Han Solo shoots {}. BECAUSE HAN SHOOTS FIRST.'.format(self.name)
+        
+        return '{} shoots {}.'.format(self.name, other.name)
+        
+        
+            
+            
+            
+        
     	
